99a23be2747d52cfa42d174be8aedb19d8f81e3c
yihanwang <yihanwang@192.168.0.6>
2018-02-16 11:07:50 +1100

    automatically read the file and return the largest number

--- src/course/Course.java
+++ src/course/Course.java
@@ -5,10 +5,18 @@
  */
 package course;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.CopyOnWriteArrayList;
-
-
+import org.apache.poi.hssf.usermodel.HSSFSheet;
+import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.FormulaEvaluator;
+import org.apache.poi.ss.usermodel.Row;
 
 
 /**
@@ -18,46 +26,51 @@ import java.util.concurrent.CopyOnWriteArrayList;
 public class Course {
     
     private ArrayList<Pair<Integer,Integer>> newArray = new ArrayList<Pair<Integer,Integer>>(); 
-    private CopyOnWriteArrayList<Integer> intList = new CopyOnWriteArrayList<Integer>();
+    //private CopyOnWriteArrayList<Integer> intList = new CopyOnWriteArrayList<Integer>();
    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception{
         // TODO code application logic here
+        Course course = new Course();
         
+        System.out.println(course.findNewCourse(course.readFile()));
     }
     
-    public int findCourse(int number){
-        newArray.add(new Pair(2,1));
-        newArray.add(new Pair(2,6));
-        newArray.add(new Pair(2,11));
-        newArray.add(new Pair(3,1));
-        newArray.add(new Pair(3,6));
-        newArray.add(new Pair(3,7));
-        newArray.add(new Pair(3,8));
-        newArray.add(new Pair(3,12));
-        newArray.add(new Pair(4,1));
-        newArray.add(new Pair(4,3));
-        newArray.add(new Pair(4,9));
-        newArray.add(new Pair(5,1));
-        newArray.add(new Pair(5,3));
-        newArray.add(new Pair(5,4));
-        newArray.add(new Pair(5,12));
-        newArray.add(new Pair(6,1));
-        newArray.add(new Pair(7,6));
-        newArray.add(new Pair(8,6));
-        newArray.add(new Pair(8,7));
-        newArray.add(new Pair(8,12));
-        newArray.add(new Pair(9,5));
-        newArray.add(new Pair(11,8));
-        newArray.add(new Pair(11,10));
+    public ArrayList<Pair<Integer,Integer>> readFile() throws IOException{
+        
+        FileInputStream fis = new FileInputStream(new File("./prerequisites.xls"));
+        //create workbook instance that refers to .xlsx file
+        HSSFWorkbook wb = new HSSFWorkbook(fis);
+        //create a shhet object to retrive the sheet;
+        HSSFSheet sheet = wb.getSheetAt(0);
+        //that is for evaluating the cell type;
+        FormulaEvaluator forlulaEvaluator = wb.getCreationHelper().createFormulaEvaluator();
+        
+        int lastrownum = sheet.getLastRowNum();
+        for(int i =1; i < lastrownum + 1;i++){
+            Row row = sheet.getRow(i);
+            Cell cell1 = row.getCell(0);
+            Cell cell2 = row.getCell(1);
+            int number1 = (int)cell1.getNumericCellValue();
+            int number2 = (int)cell2.getNumericCellValue();
+            newArray.add(new Pair(number1,number2));
+        }
+        return newArray;
+    }
+    
+    /*
+    public int findCourse(int courseId) throws IOException{
+        readFile();
+        
         int i = 0;
-      
+        //first loop to check the course id in the array
         while(i<newArray.size()){
-            if(newArray.get(i).first == number){
+            if(newArray.get(i).first == courseId){
                 intList.add(newArray.get(i).second);
-                
+                //then loop the new arrayList to check the course id 
+                //until all prerequisites are added in the new arrayList
                 for(int n = 0;n < intList.size();n++){
                     int c = 0;
                     while(c<newArray.size()){
@@ -75,7 +88,49 @@ public class Course {
         }
         return intList.size();
     }
-    
+    */    
+
+    public int findNewCourse(ArrayList<Pair<Integer,Integer>> courseArray){
+        int preNumber = 0;
+        //CopyOnWriteArrayList<Integer> intList = new CopyOnWriteArrayList<Integer>();
+        CopyOnWriteArrayList<Integer> allCourse = new CopyOnWriteArrayList<Integer>();
+        
+        for(int courseId = 0; courseId < courseArray.size();courseId++){
+            allCourse.add(courseArray.get(courseId).first);
+        }
+        
+        //first loop to check the course id in the array
+        for(int o: allCourse){
+            CopyOnWriteArrayList<Integer> intList = new CopyOnWriteArrayList<Integer>();
+            int i = 0;
+            while(i < newArray.size()){
+                    if(newArray.get(i).first == o){
+                    intList.add(newArray.get(i).second);
+                    //then loop the new arrayList to check the course id 
+                    //until all prerequisites are added in the new arrayList
+                     for(int n = 0;n < intList.size();n++){
+                        int c = 0;
+                        while(c<newArray.size()){
+                            if(newArray.get(c).first == intList.get(n)){
+                                if(!intList.contains(newArray.get(c).second)){
+                                    intList.add(newArray.get(c).second);
+                                    int n2 = intList.size();
+                                }
+                            }
+                            c++;
+                        }
+                    }
+                }
+                i++;
+            }
+            if(preNumber < intList.size()){
+                //System.out.println(intList.size());
+                preNumber = intList.size();
+            }
+            
+        }
+        return preNumber;
+    }
     
 }
 
